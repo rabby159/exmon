3c7b56c76db9e6886bd97b12ac6e966079e385c1
Md Rabby <ablazerabbi159@gmail.com>
2024-05-21 01:45:48 +0600

    eslint error issue fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@
 
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import globals from "globals";
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -9,7 +10,6 @@ export default tseslint.config(
   {
     languageOptions: {
         globals: {
-            // @ts-ignore
             ...globals.node,
         },
     },
