fee051786b9a7cf581b4206125374d800969a3a2
Md Rabby <ablazerabbi159@gmail.com>
2024-05-21 01:39:38 +0600

    initial project setup complete

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+        globals: {
+            // @ts-ignore
+            ...globals.node,
+        },
+    },
+  },
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error",
+      "prefer-const": "error",
+      "no-console": "warn"
+    },
+  }
+);
